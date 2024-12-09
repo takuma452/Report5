1501a1fe7d6ff1e66ada7f4be9c2c654466d04f9
Uehara Takuma <e245743@ie.u-ryukyu.ac.jp>
2024-12-09 11:27:32 +0900

    third commit

--- src/jp/ac/ryukyu/u/ie/e245743/Exception.java
+++ src/jp/ac/ryukyu/u/ie/e245743/Exception.java
@@ -1,14 +1,8 @@
 package jp.ac.ryukyu.u.ie.e245743;
 
-public class Exception {
-  public static void main(String[] args) {
-    String str = null;
-
-    try{
-      System.out.println(str.length());
-    } catch(NullPointerException e){
-      System.out.println("NullPointerExceptionが発生しました");
-      System.out.println(e.getMessage());
+public class Exception{
+    public static void main(String[] args) {
+      String str = "壱百満";
+      int value = Integer.parseInt(str);
     }
-  }
 }
